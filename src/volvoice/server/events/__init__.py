4e813ad9e4a8ecc13a7b6e54a023b48d3f537406
Anton <an.volovod@yandex.ru>
2025-10-23 12:26:28 +0400

    структура

--- src/volvoice/server/events/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Application lifecycle event handlers."""
-
-from .startup import register_startup_event
-
-__all__ = ["register_startup_event"]
--- src/volvoice/services/text/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Text channel service placeholder."""
-
-from .service import TextChannelService, get_text_channel_service
-
-__all__ = ["TextChannelService", "get_text_channel_service"]
